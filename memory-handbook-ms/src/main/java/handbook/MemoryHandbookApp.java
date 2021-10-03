47e84f2bf6719be4566f329f2dae5ddf46024673
Michele <micheleperilli.98@libero.it>
2021-10-03 10:45:49 +0200

    Configurazione iniziale swagger

--- memory-handbook-ms/src/main/java/handbook/MemoryHandbookApp.java
+++ memory-handbook-ms/src/main/java/handbook/MemoryHandbookApp.java
@@ -2,14 +2,12 @@ package handbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableSwagger2
-@EnableConfigurationProperties
 public class MemoryHandbookApp {
 
     public static void main(String[] args) {
--- memory-handbook-ms/src/main/java/handbook/conrtoller/PasswordController.java
+++ memory-handbook-ms/src/main/java/handbook/conrtoller/PasswordController.java
@@ -2,14 +2,11 @@ package handbook.conrtoller;
 
 import handbook.model.dto.RicercaPasswordResponseDto;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
 
 @RestController
 @RequestMapping(value = {"/password"})
