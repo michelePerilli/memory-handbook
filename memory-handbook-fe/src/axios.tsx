3bd8f9e66078d5f880ff9ded7bb3420eaecc4e33
Michele Perilli <micheleperilli.98@libero.it>
2025-10-05 22:48:49 +0200

    rimosso frontend

--- memory-handbook-fe/src/axios.tsx
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-        baseURL: 'http://localhost:8081/api',
-        headers: {
-            "Access-Control-Allow-Origin": "*"
-        }
-    }
-)
--- memory-handbook-fe/src/model/password-dto.tsx
+++
@@ -1,9 +0,0 @@
-interface PasswordDto {
-    descrizione: string
-    email: string
-    id: number
-    password: string
-    username: string
-}
-
-export default PasswordDto;
